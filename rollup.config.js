b2a86918f982212accf2fa48dcc8cc403fe3bed8
Nathan Faucett <nathanfaucett@gmail.com>
2021-09-01 18:24:42 -0400

    single export for browser

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,13 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
 import esmImportToUrl from "rollup-plugin-esm-import-to-url";
-import pkg from "./package.json";
 
 export default [
   {
     input: "src/index.ts",
     output: [
       {
-        file: pkg.module,
+        file: "browser/index.js",
         format: "es",
         sourcemap: true,
         plugins: [terser()],
@@ -20,10 +19,13 @@ export default [
       esmImportToUrl({
         imports: {
           tslib: "https://unpkg.com/tslib@2/tslib.es6.js",
+          "@aicacia/core": "https://unpkg.com/@aicacia/core@0/browser/index.js",
         },
       }),
       resolve({ browser: true }),
-      commonjs(),
+      commonjs({
+        transformMixedEsModules: true,
+      }),
       typescript({
         tsconfig: "./tsconfig.esm.json",
       }),
